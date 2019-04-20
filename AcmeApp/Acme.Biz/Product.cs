31eda274523d8d804c9273ba3ea2a6d4265fc810
mmonirul <mohammadmonirul@gmail.com>
2019-04-20 21:22:12 +0200

    Update AcmeApp/Acme.Biz/Product.cs

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -3,7 +3,7 @@
     public class Product
     {
         public string Description { get; set; }
-        public int ProductId { get; set; }
+        public int Id { get; set; }
         public string ProductName { get; set; }
     }
 }
