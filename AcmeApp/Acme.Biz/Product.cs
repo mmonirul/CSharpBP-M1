e5278b2bfe2d2f0b7ccff15f3d825ca9af57a544
Monirul <monirul.islam@telenor.se>
2019-04-20 21:12:38 +0200

    Added new product class

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Acme.Biz
+{
+    public class Product
+    {
+        public string Description { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
