6dfc31bcd36d39961e6c10815dd06f8b6e8c4d65
mmonirul <mohammadmonirul@gmail.com>
2019-04-20 21:25:14 +0200

    Sort prop

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -2,8 +2,8 @@
 {
     public class Product
     {
-        public string Description { get; set; }
         public int Id { get; set; }
+        public string Description { get; set; }
         public string ProductName { get; set; }
     }
 }
